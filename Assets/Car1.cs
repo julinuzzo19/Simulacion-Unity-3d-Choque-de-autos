ecdd48a092906c924a78f22c223307dda67ce779
julinuzzo19 <julinuzzo19@gmail.com>
2022-05-15 17:12:04 -0300

    first commit

---
+++ Assets/Car1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car1 : MonoBehaviour
+{
+
+    private Vector3 PosicionInicial;
+    private Vector3 PosicionFinal;
+    public Vector3 PosicionDestino;
+
+    public float speed= 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PosicionInicial = transform.position;
+        PosicionFinal = PosicionInicial + PosicionDestino * Time.deltaTime * speed;
+        transform.position = PosicionFinal;
+         
+    }
+}
